8dc9b6b006ba85c7df21611f0b1d49ee6b73916e
lizkes <erfan2333@gmail.com>
2021-02-07 11:21:33 +0800

    适配新的参数

--- check_traffic.py
+++ check_traffic.py
@@ -16,9 +16,9 @@ def transmissionrate(dev, direction, timestep):
     return (bytes_after - bytes_before) / timestep
 
 
-byterate = transmissionrate("eth0", "tx", 30) / 30
+byterate = transmissionrate("eth0", "tx", 10) / 10
 print(f"tx byterate: {byterate}")
-while byterate > 32 * 1024:  # 32KB/s
-    byterate = transmissionrate("eth0", "tx", 30) / 30
+while byterate > 16 * 1024:  # 16KB/s
+    byterate = transmissionrate("eth0", "tx", 10) / 10
     print(f"tx byterate: {byterate}")
 print("tx traffic is low, Task is Done, Exit")
